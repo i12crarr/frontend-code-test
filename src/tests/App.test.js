21d497ed8a7f01e46821c70cd5a0c54fe5967cc4
Rafa <rafacr93@gmail.com>
2023-07-30 17:42:12 +0200

    feat: test dragging and changing color for selected boxes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -83,7 +83,7 @@ describe("Delete the last added box inside the canvas", () => {
 
   // TEST CHANGE COLOR
 
-  describe("MainStore", () => {
+  describe("Change color of a box", () => {
     test("should change the color of a box", () => {
       const store = MainStore.create();
       const newBox = BoxModel.create({
@@ -107,7 +107,7 @@ describe("Delete the last added box inside the canvas", () => {
 
   // TEST COUNTER SELECTED BOXES
 
-  describe("MainStore", () => {
+  describe("Show the correct number of selected boxes", () => {
     it("should count the number of selected boxes correctly", () => {
       const store = MainStore.create();
   
@@ -141,5 +141,65 @@ describe("Delete the last added box inside the canvas", () => {
       expect(store.arrayBoxes.length).toBe(1);
       store.changeStateSelected(box2);
       expect(store.arrayBoxes.length).toBe(0);
+    });
+  });
+  
+  // TEST DRAGGING BOXES
+
+  describe("Move selected boxes and change color", () => {
+    test("should move and change color of the selected boxes correctly", () => {
+
+      const store = MainStore.create();
+  
+      const box1 = BoxModel.create({
+        id: "box-1",
+        color: "#0059FF",
+        left: 100,
+        top: 100,
+      });
+      const box2 = BoxModel.create({
+        id: "box-2",
+        color: "#7FABFF",
+        left: 200,
+        top: 200,
+      });
+      const box3 = BoxModel.create({
+        id: "box-3",
+        color: "#BED4FE",
+        left: 300,
+        top: 300,
+      });
+
+      store.addBox(box1);
+      store.addBox(box2);
+      store.addBox(box3);
+  
+ 
+      store.changeStateSelected(box1);
+      store.changeStateSelected(box3);
+  
+      store.changeColor('#FEEEBE');
+      // Move the selected boxes
+      store.moveBoxesSelected(80, 80);
+  
+      // Check that the positions of the first and third boxes are updated correctly
+      expect(box1.left).toBe(180);
+      expect(box1.top).toBe(180);
+      expect(box3.left).toBe(380);
+      expect(box3.top).toBe(380);
+  
+      // Check that color has change for the selected boxes
+
+      expect(box1.color).toBe('#FEEEBE');
+      expect(box3.color).toBe('#FEEEBE');
+
+      // Deselect the boxes
+      store.changeStateSelected(box1);
+      store.changeStateSelected(box3);
+  
+      // Check that the position and color of the second box is not updated because it's not selected
+      expect(box2.left).toBe(200);
+      expect(box2.top).toBe(200);
+      expect(box2.color).not.toBe('#FEEEBE');
     });
   });
